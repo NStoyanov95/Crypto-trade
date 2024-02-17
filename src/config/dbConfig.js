b58e94454ab68347084feb3373f908e33048f224
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 15:37:18 +0200

    Connect db

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 //CHANGE DB NAME
 const dbConfigurator = async () =>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/second-hand-electronics');
+    await mongoose.connect('mongodb://127.0.0.1:27017/crypto-trade');
 };
 
 module.exports = dbConfigurator;
