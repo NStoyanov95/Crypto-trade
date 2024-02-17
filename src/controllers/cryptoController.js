91595a6ccd0b2d8686a493eed1d8f9afc6472367
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 16:32:18 +0200

    Show catalog page with dynamic data

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -4,20 +4,29 @@ const cryptoService = require('../services/cryptoService');
 
 const { getErrorMessage } = require('../utils/errorUtils');
 
-router.get('/create',(req,res)=>{
+router.get('/create', (req, res) => {
     res.render('crypto/create')
 });
 
-router.post('/create', async(req,res)=>{
+router.post('/create', async (req, res) => {
     const cryptoData = req.body
     try {
         await cryptoService.create(cryptoData);
         res.redirect('/crypto/catalog')
     } catch (error) {
-        res.render('crypto/create', {error: getErrorMessage(error)})
+        res.render('crypto/create', { error: getErrorMessage(error) })
     }
 });
 
+router.get('/catalog', async (req, res) => {
+    try {
+        const crypto = await cryptoService.getAll().lean();
+        res.render('crypto/catalog', { crypto });
+
+    } catch (error) {
+        res.redirect('/404');
+    }
+})
 
 
 module.exports = router;
--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -2,3 +2,5 @@ const Crypto = require('../models/Crypto');
 
 
 exports.create = (cryptoData) => Crypto.create(cryptoData);
+
+exports.getAll = () => Crypto.find();
