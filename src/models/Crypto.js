e3abb130dd70f8e4e8375d33a23a7fdffcaabe72
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 17:49:55 +0200

    Add crypto model validaton

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -4,23 +4,27 @@ const mongoose = require('mongoose');
 const cryptoSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Please enter name'],
+        minLength: [2, 'Name should be at least 2 characters long']
     },
     image: {
         type: String,
-        required: true
+        required: [true, 'Please enter image'],
+        match: [/^https?:\/\//, 'Image URL should start with http:// or https://']
     },
     price: {
         type: Number,
-        required: true
+        required: [true, 'Please enter price'],
+        min: [0, 'Price should be positive number']
     },
     cryptoDescription: {
         type: String,
-        required: true
+        required: [true, 'Please enter description'],
+        minLength: [10, 'Description should be at least 10 characters long']
     },
     paymentMethod: {
         type: String,
-        required: true,
+        required: [true, 'Please enter payment method'],
         enum: {
             values: ['crypto-wallet', 'credit-card', 'debit-card', 'paypal'],
             message: 'Payment method should be one of the following: crypto-wallet, credit-card, debit-card, paypal'
