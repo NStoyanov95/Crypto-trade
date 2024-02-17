82c054ca07028e785f716d9b1b3a815549eff616
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 15:51:42 +0200

    Add crypto model

---
+++ src/models/Crypto.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+
+const cryptoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    cryptoDescription: {
+        type: String,
+        required: true
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        enum: {
+            value: ['crypto-wallet', 'credit-card', 'debit-card', 'paypal'],
+            message: 'Payment method should be one of the following: crypto-wallet, credit-card, debit-card, paypal'
+        }
+    },
+    buyACrypto: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+
+module.exports = Crypto;
